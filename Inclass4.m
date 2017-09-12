3b091d234883abeb69bce6d6e50688f6fa01d55a
mestrada1 <31257576+mestrada1@users.noreply.github.com>
2017-09-12 03:00:55 -0500

    Add files via upload

--- Inclass4.m
+++ Inclass4.m
@@ -6,6 +6,53 @@
 %that reads only the first line of random numbers in the file and stores
 %them as numbers in an array. 
 
+% (a). 
+filename = 'RandomNumbers.txt';
+fid = fopen(filename, 'w');
+fprintf(fid, 'Random numbers 1\n');
+for i=1:10
+    line1=randi(10,1,13);
+    fprintf(fid, '%i %i %i %i %i %i %i %i %i %i %i %i %i\n', line1);
+end
+fprintf(fid, 'Random numbers 2\n');
+for i=1:9
+    line1=randi(10,1,7);
+    fprintf(fid, '%i %i %i %i %i %i %i\n', line1);
+end
+
+% (b).
+fid=(fopen('RandomNumbers.txt','r'));
+line1=fgetl(fid);
+line1=[]
+line1=fgetl(fid);
+line1split = strsplit(line1, ' ');
+numberarray = cellfun(@str2num, line1split);
+
+
 % 2. Write a function that takes an array as input and returns a logical
 % variable which is true if the sum of the numbers in the array is greater
 % than or equal to 10 and false if it is less than 10. 
+
+function result = sumarray10(x)
+    result = sum(x)>= 10
+end
+
+a=1:5;
+
+sumarray10(a)
+
+result =
+
+  logical
+
+   1
+
+w=[1 3 3 2];
+
+sumarray10(w)
+
+result =
+
+  logical
+
+   0
